13ec6832009e90674dde56b0216cf40c6d29998f
Zheng-Yi-git <923408911@qq.com>
2021-12-06 19:09:31 +0800

    数据库操作修改

--- oracle.py
+++ oracle.py
@@ -1,7 +1,8 @@
+from tkinter.constants import FALSE
 import cx_Oracle as oracle # 引入oracle数据库模块
 #32位的Oracle系统可以通过安装instantclient并运行下面两行代码成功运行在64位的python环境，记得修改路径！
-#import os
-#os.environ['path'] =  r'D:/Codefield/CODE_python/instantclient_21_3'
+import os
+os.environ['path'] =  r'D:/Codefield/CODE_python/instantclient_21_3'
 
 
 # ------------------------ 通用函数 ----------------------
@@ -15,6 +16,28 @@ def msg(pattern,title,content):
     elif(pattern=='err'):
         messagebox.showerror(title=title,message=content)
 
+
+def inspect(txt,var,length,*limit): # 输入格式判断 
+
+    # 参数：文本，类型，定长值，*最短值，*最长值
+
+    if(var=='int'):
+        for i in txt:
+            if(i<'0' or i>'9'): 
+                msg('err','错误','只能输入数字！')
+                return True
+    l=len(txt)
+    if(length!=0): 
+        if(l!=length):
+            msg('err','错误','应当输入%d个字符！'%(length))
+            return True
+    elif(l<limit[0]):
+        msg('err','错误','至少输入%d个字符！'%(limit[0]))
+        return True
+    elif(l>limit[1]):
+        msg('err','错误','至多输入%d个字符！'%(limit[1]))
+        return True
+    return False
 
 # ----------------------- 数据库连接 ----------------------
 
@@ -40,3 +63,42 @@ def finish(flag): # 关闭连接
 
 #------------------------- 登录界面 ---------------------------
 
+
+def check(account, password):    #登录检验
+    try:
+        cursor.execute("select * from users where account='%s' and password='%s'" %(account,password))
+        res = cursor.fetchone()
+        if (res == None):
+            msg('err', '错误', '账号不存在或密码错误！')
+            return 0
+        else:
+            return res
+    except oracle.DatabaseError as e:
+        msg('err', '错误', str(e))
+
+
+def sign_in(name, age, sex, password, confirm):      #提交注册（return 根据main函数需求更改，更改完后可以删去本注释）
+    if (password != confirm):
+        msg('err', '错误', '两次输入密码不一致！')
+        # return (FALSE, 2) 
+    else:
+        try:
+            #设置一个默认初始值，空表插入用默认值作为账号，其他则用此账号依次加一
+            cursor.execute("insert into users values('%s', %d, '%s', '%s', '%s')" %(name, age, sex, password, confirm))
+            commit()
+            msg('inf','提示','注册成功！')
+            # return (True,)
+        except oracle.DatabaseError as e:
+            msg('err','错误',str(e))
+            # return (False,0)
+
+
+#----------------------------操作部分-----------------------------------
+def search(t, *arg):
+    # t == 0 赛事门票查询
+    # t == 1 商品查询
+
+
+
+def update(t, *arg):
+    # t == 0 志愿者服务批准
